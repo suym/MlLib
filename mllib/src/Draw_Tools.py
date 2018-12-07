4ed9929b71e10ed612c90dffe8db1bb4ae3ea414
suym <suyumo@buaa.edu.cn>
2018-12-07 11:50:47 +0800

    增加散点图和饼图

---
+++ mllib/src/Draw_Tools.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = "Su Yumo <suyumo@buaa.edu.cn>"
+
+import pygal              
+from collections import Counter
+
+def Read_info(dir_of_dict):
+    '''
+    读取配置文件
+    '''
+    with open(dir_of_dict,'r') as f:
+        column_lines = f.read()
+        name_dict = eval(column_lines)
+    A_func_B = ['AB_scatter',]
+    A_func = ['A_pie',]
+
+    options = name_dict['options']
+    task_id = name_dict['task_id']
+    job_id = name_dict['job_id']
+    dir_of_inputdata = name_dict['dir_of_inputdata']
+    dir_of_outputdata = name_dict['dir_of_outputdata']
+    data_size = name_dict['data_size']
+
+    if options in A_func_B:
+        A_col = name_dict['A_col']
+        B_col = name_dict['B_col']
+        bag = options,task_id,job_id,dir_of_inputdata,dir_of_outputdata,data_size,A_col,B_col
+        
+    if options in A_func:
+        A_col = name_dict['A_col']
+        bag = options,task_id,job_id,dir_of_inputdata,dir_of_outputdata,data_size,A_col
+
+    return bag
+
+def AB_scatter(data,A,B):
+    xy = zip(data[A],data[B])
+    xy_chart = pygal.XY(stroke=False)
+    xy_chart.title = 'Correlation'
+    xy_chart.x_title = A
+    xy_chart.y_title = B
+    xy_chart.add('%s-%s'%(A,B), xy)
+    return xy_chart
+
+def A_pie(data,A):
+    x = dict(Counter(data[A]))
+    pie_chart = pygal.Pie()
+    pie_chart.title = '%s Pie'%A  
+    for key,value in x.items():
+        pie_chart.add(str(key), value)
+    return pie_chart 
---
+++ mllib/visualData/Scatter_diagram.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = "Su Yumo <suyumo@buaa.edu.cn>"
+
+import sys  
+sys.path.append("..")
+import pandas as pd
+from src import Draw_Tools as dt
+
+def main():
+    dir_of_dict = sys.argv[1]
+    bag = dt.Read_info(dir_of_dict)
+    options,task_id,job_id,dir_of_inputdata,\
+    dir_of_outputdata,data_size,A_col,B_col = bag
+    #dir_of_outputdata = dir_of_outputdata + '/%s.svg'%(str(task_id)+'_'+str(job_id)+'_'+options)
+
+    dataset = pd.read_csv(dir_of_inputdata)
+    if data_size < len(dataset):
+        dataset = dataset.sample(n=data_size,replace=False,axis=0)
+    if options == 'AB_scatter':
+        diagram = dt.AB_scatter(dataset,A_col,B_col)
+        diagram.render_to_file(dir_of_outputdata)
+
+if __name__ == '__main__':
+    main()
